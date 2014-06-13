16eb76d473f86f5be34b3ac78744df4ac9154f8e
Andreas Galauner <andreas@galauner.de>
2014-06-13 15:22:00 +0200

    Initial import

---
+++ static/js/autoc4.js
@@ -0,0 +1,5 @@
+$(function(){
+	Raphael.colorwheel($("#dmxcolorplenar") ,250).color("#F00");
+	Raphael.colorwheel($("#dmxcolorwohnzimmer") ,250).color("#F00");
+	Raphael.colorwheel($("#dmxcolorfnordcenter") ,250).color("#F00");
+});
