5707f25b7e85e5db57cf5f45018c6b7c2309ad62
Florian Zeitz <florob@babelmonkeys.de>
2014-06-13 19:43:33 +0200

    Make lights toggle-able in the UI

--- static/js/autoc4.js
+++ static/js/autoc4.js
@@ -2,4 +2,8 @@ $(function(){
 	Raphael.colorwheel($("#dmxcolorplenar") ,250).color("#F00");
 	Raphael.colorwheel($("#dmxcolorwohnzimmer") ,250).color("#F00");
 	Raphael.colorwheel($("#dmxcolorfnordcenter") ,250).color("#F00");
+
+	$(".btn-light").click(function (e) {
+		$(this).toggleClass("on");
+	});
 });
