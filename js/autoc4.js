1f7f2b6573ededf009a3bb0a518417b1f66a6645
root <root@autoc4.labor.koeln.ccc.de>
2019-01-28 23:56:38 +0100

    hard coded local ip address

--- js/autoc4.js
+++ js/autoc4.js
@@ -20,7 +20,8 @@
 var mqtt_client;
 
 function init_mqtt() {
-    mqtt_client = new Paho.MQTT.Client(location.hostname, 9000, mqtt_generate_clientid());
+    //mqtt_client = new Paho.MQTT.Client(location.hostname, 9000, mqtt_generate_clientid());
+    mqtt_client = new Paho.MQTT.Client('172.23.23.110', 9000, mqtt_generate_clientid());
     mqtt_client.onMessageArrived = mqtt_on_message;
     mqtt_client.onConnectionLost = mqtt_on_connect_failure;
     mqtt_client.connect({onSuccess: mqtt_on_connect, onFailure: mqtt_on_connect_failure, mqttVersion: 3});
