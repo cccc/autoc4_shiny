a4a37f5b879b0fd5355d48106f36339703847f56
Necro <andre.kramp@necro-development.net>
2019-04-14 20:48:27 +0200

    fixed hsv to rgb function

--- js/autoc4_dmx.js
+++ js/autoc4_dmx.js
@@ -199,13 +199,15 @@ var autoc4_dmx = function (){
 }
 
 autoc4_dmx.hsv_to_rgb = function(h, s, v) {
-    var h_i = Math.floor(h*6);
+    var r, g, b;
+
+    var i = Math.floor(h * 6);
     var f = h * 6 - i;
     var p = v * (1 - s);
     var q = v * (1 - f * s);
     var t = v * (1 - (1 - f) * s);
-    var r, g, b;
-    switch (h_i) {
+
+    switch (i % 6) {
         case 0: r = v, g = t, b = p; break;
         case 1: r = q, g = v, b = p; break;
         case 2: r = p, g = v, b = t; break;
@@ -213,7 +215,8 @@ autoc4_dmx.hsv_to_rgb = function(h, s, v) {
         case 4: r = t, g = p, b = v; break;
         case 5: r = v, g = p, b = q; break;
     }
-    return [Math.floor(r*255), Math.floor(g*255), Math.floor(b*255)];
+
+    return [ r * 255, g * 255, b * 255 ];
 }
 
 
