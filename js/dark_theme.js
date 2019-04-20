dfe79b8c348be6d38c1338d90ff923a719a38e86
Necro <andre.kramp@necro-development.net>
2019-04-20 11:36:10 +0200

    some tweaks to dark theme and html structure

--- js/dark_theme.js
+++ js/dark_theme.js
@@ -1,2 +1,16 @@
-if(new URLSearchParams(window.location.search).get('dark')!=null)
-    $("body").addClass("dark");
+(function(window, $){
+    var $body = $("body");
+    var searchParams = new URLSearchParams(window.location.search);
+
+    if(searchParams.get('dark')!=null)
+        $body.addClass("dark");
+    $(function(){
+        $body.on("click","[data-toggle=theme]",function(){
+            if($body.hasClass("dark"))
+                searchParams.delete("dark");
+            else
+                searchParams.set("dark","");
+            window.location.search = searchParams.toString();
+        });
+    })
+})(window, jQuery)
