f672b7c0208a61d32b6a2b65a734cebfc122a0b4
Necro <andre.kramp@necro-development.net>
2019-12-21 16:02:34 +0100

    refactored club state module

--- assets/js/autoc4/state.js
+++ assets/js/autoc4/state.js
@@ -1,32 +1,32 @@
 class AutoC4State {
     init(autoc4, options) {
         this.autoc4 = autoc4;
+        this.options = options;
         return this;
     }
     onMessage(autoc4, message) {
+        let targets = document.querySelectorAll(this.options.target);
         let icon = $('#club-status .fa');
         let text = $('#club-status :last-child');
         if (message.payloadBytes[0]) {
-            icon.removeClass('fa-hand-point-right fa-thumbs-down fa-exclamation-circle')
-                .addClass('fa-thumbs-up');
-            icon.css('color', '#0c0');
-            text.text('Open');
+            targets.forEach((e) => {
+                e.classList.remove(this.options.closedClass, this.options.disconnectedClass);
+                e.classList.add(this.options.openClass);
+            });
         }
         else {
-            icon.removeClass('fa-hand-point-right fa-thumbs-up fa-exclamation-circle')
-                .addClass('fa-thumbs-down');
-            icon.css('color', '#c00');
-            text.text('Closed');
+            targets.forEach((e) => {
+                e.classList.remove(this.options.openClass, this.options.disconnectedClass);
+                e.classList.add(this.options.closedClass);
+            });
         }
     }
     onConnect(autoc4, o) { }
     onConnectionFailure(autoc4, error) {
-        let icon = $('#club-status .fa');
-        let text = $('#club-status :last-child');
-        icon.removeClass('fa-thumbs-down fa-thumbs-up')
-            .addClass('fa-exclamation-circle');
-        icon.css('color', '#a00');
-        text.text('Disconnected');
+        document.querySelectorAll(this.options.target).forEach((e) => {
+            e.classList.remove(this.options.openClass, this.options.openClass);
+            e.classList.add(this.options.disconnectedClass);
+        });
     }
 }
 AutoC4.registerModule("state", () => new AutoC4State());
