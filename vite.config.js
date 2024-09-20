2e9efc5d32e015b80d7096e9e5b86bf969e3e671
Qb <qb@qbit.moe>
2024-09-20 17:15:05 +0200

    Aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import injectHtml from "vite-plugin-html-inject";
+import path from "path";
 
 export default defineConfig({
 	base: "./",
 	plugins: [injectHtml()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		}
+	}
 });
